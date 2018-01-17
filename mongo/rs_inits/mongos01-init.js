83d0f62aa2201356998857037994971699737d6d
y_wakayama <y_wakayama@y-wakayamanoMacBook-Pro.local>
2018-01-17 18:19:01 +0900

    fix mongos config

--- mongo/rs_inits/mongos01-init.js
+++ mongo/rs_inits/mongos01-init.js
@@ -1,2 +1,4 @@
 sh.addShard("rs2/mongosrd_rs2_1:27017");
+sh.addShard("rs2/mongosrd_rs2_2:27017");
+sh.addShard("rs2/mongosrd_rs2_3:27017");
 
--- mongo/rs_inits/mongos02-init.js
+++ mongo/rs_inits/mongos02-init.js
@@ -1,1 +1,3 @@
+sh.addShard("rs2/mongosrd_rs2_1:27017");
 sh.addShard("rs2/mongosrd_rs2_2:27017");
+sh.addShard("rs2/mongosrd_rs2_3:27017");
